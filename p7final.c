83d9c9f40ad628d8d27991c9d165b7129672e88d
Vivekanand Shridhar Deshinge <97461910+Maharshivivekanand@users.noreply.github.com>
2022-03-03 08:48:37 +0000

    gcd done

--- p7final.c
+++ p7final.c
@@ -1,1 +1,20 @@
 
+
+/*Create invite link and paste link in the form and submit form. ( You can edit response later). https://docs.google.com/forms/d/e/1FAIpQLSdTOayhPycdDURXRHTDZ8CDeLmTS9s-sTkkM_VTf_sCzD33uA/viewform?usp=sf_link*/
+
+#include <stdio.h>
+int find_gcd(int a,int b)
+{
+  int t;
+  while (b !=0)
+    {
+      t=b;
+      b=a%b;
+      a=t;
+    }
+  return a;
+}
+int main()
+{
+  printf("%d\n",find_gcd(16,24));
+}
